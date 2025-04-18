3c9d0d3af6060b48ab6b3911bbe0ece7afcbac4e
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-04-18 16:24:47 +0700

    pelayanan

--- app/Http/Controllers/PelayananController.php
+++ app/Http/Controllers/PelayananController.php
@@ -8,8 +8,14 @@ use Illuminate\Support\Facades\DB;
 class PelayananController extends Controller
 {
     public function index() {
-        DB::insert('insert into t_pelayanan(jenis_pelayanan, harga_pelayanan, created_at) values(?, ?, ?)', ['Recolor', '120000', now()]);
+        // DB::insert('insert into t_pelayanan(jenis_pelayanan, harga_pelayanan, created_at) values(?, ?, ?)', ['Recolor', '120000', now()]);
 
-        return 'Insert data baru berhasil';
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::update('update t_pelayanan set jenis_pelayanan = ? where harga_pelayanan = ?', ['Recolor', '125000']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' .$row.' baris';
+
+        $data = DB::select('select * from t_pelayanan');
+        return view('pelayanan', ['data' => $data]);
     }
 }
